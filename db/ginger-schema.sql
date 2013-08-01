2686e9534b403576f188dcd583dbad368899602d
Codeliner <kontakt@codeliner.ws>
2013-08-02 00:30:30 +0200

    Add db scripts
    Add db schema
    Add some testdata

---
+++ db/ginger-schema.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 02. Aug 2013 um 00:26
+-- Server Version: 5.5.31
+-- PHP-Version: 5.4.17RC1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `ginger`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `configuration`
+--
+
+CREATE TABLE IF NOT EXISTS `configuration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_id` int(11) NOT NULL,
+  `config` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `configuration_run`
+--
+
+CREATE TABLE IF NOT EXISTS `configuration_run` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_run_id` int(11) NOT NULL,
+  `configuration_id` int(11) NOT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `total_item_count` int(11) NOT NULL DEFAULT '0',
+  `inserted_item_count` int(11) NOT NULL DEFAULT '0',
+  `success` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=78 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `configuration_run_message`
+--
+
+CREATE TABLE IF NOT EXISTS `configuration_run_message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `configuration_run_id` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `type` enum('info','warning','error') NOT NULL,
+  `text` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=81 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `connector_element`
+--
+
+CREATE TABLE IF NOT EXISTS `connector_element` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `class` text NOT NULL,
+  `link` text NOT NULL,
+  `module_name` varchar(100) NOT NULL,
+  `type` enum('source','target','mapper','feature') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UniqueNameType` (`name`,`type`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=45 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `job`
+--
+
+CREATE TABLE IF NOT EXISTS `job` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  `break_on_failure` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `job_run`
+--
+
+CREATE TABLE IF NOT EXISTS `job_run` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_id` int(11) NOT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `success` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=67 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `module_configuration`
+--
+
+CREATE TABLE IF NOT EXISTS `module_configuration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `module` varchar(200) NOT NULL,
+  `configuration` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `module` (`module`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `permission`
+--
+
+CREATE TABLE IF NOT EXISTS `permission` (
+  `user_id` int(11) NOT NULL,
+  `job_name` varchar(100) NOT NULL,
+  `read` tinyint(1) NOT NULL DEFAULT '0',
+  `write` tinyint(1) NOT NULL DEFAULT '0',
+  `execute` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`,`job_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `api_key` varchar(32) NOT NULL,
+  `secret_key` text NOT NULL,
+  `lastname` text NOT NULL,
+  `firstname` text NOT NULL,
+  `email` text NOT NULL,
+  `is_admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `api_key` (`api_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
