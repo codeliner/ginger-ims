8bb5f9906c052c587ae19aff1c80360bb0919a2e
Codeliner <kontakt@codeliner.ws>
2013-08-10 18:38:02 +0200

    Remove unused connector elements

--- db/connector_element.sql
+++ db/connector_element.sql
@@ -26,24 +26,18 @@ SET time_zone = "+00:00";
 
 INSERT INTO `connector_element` (`id`, `name`, `class`, `link`, `module_name`, `type`) VALUES
 (1, 'TableStructureMapper', 'Ginger\\Model\\Mapper\\TableStructureMapper', '#', 'Ginger.Application', 'mapper'),
-(22, 'Digits', 'Ginger\\Model\\Feature\\ValidatorFeature', '#', 'Ginger.Application', 'feature'),
-(23, 'StringTrim', 'Ginger\\Model\\Feature\\FilterFeature', '#', 'Ginger.Application', 'feature'),
-(24, 'AttributeMap', 'Ginger\\Model\\Feature\\AttributeMapFeature', '#', 'Ginger.Application', 'feature'),
-(28, 'DocumentStructureMapper', 'Ginger\\Model\\Mapper\\DocumentStructureMapper', '#', 'Ginger.Application', 'mapper'),
-(29, 'File', 'Ginger\\Model\\File\\SourceFile', '#', 'Ginger.Application', 'source'),
-(31, 'vonWerth::categories', 'SqlConnect\\Model\\Db\\TableTarget', 'sqlconnect/targets/vonWerth/show/categories', 'SqlConnect', 'target'),
-(32, 'Null', 'Ginger\\Model\\Feature\\FilterFeature', '#', 'Ginger.Application', 'feature'),
-(33, 'Categories', 'JtlWawi\\Model\\Source\\Categories', '#', 'JtlWawi', 'source'),
-(34, 'Directory', 'Ginger\\Model\\Directory\\SourceDirectory', '#', 'Ginger.Application', 'source'),
-(35, 'vonWerth::articles', 'SqlConnect\\Model\\Db\\TableTarget', 'sqlconnect/targets/vonWerth/show/articles', 'SqlConnect', 'target'),
-(36, 'Manipulator::StaticValue', 'Ginger\\Model\\Feature\\StaticValueFeature', '#', 'Ginger.Application', 'feature'),
-(37, 'Url', 'Ginger\\Model\\Feature\\FilterFeature', '#', 'Ginger.Application', 'feature'),
-(38, 'PHP Script', 'Ginger\\Model\\Script\\SourceScript', '#', 'Ginger.Application', 'source'),
-(39, 'Dev/Null', 'Ginger\\Model\\Script\\DevNullTarget', '#', 'Ginger.Application', 'target'),
-(40, 'Directory', 'Ginger\\Model\\Directory\\TargetDirectory', '#', 'Ginger.Application', 'target'),
-(42, 'vonWerth::articles', 'SqlConnect\\Model\\Db\\TableSource', 'sqlconnect/sources/vonWerth/show/articles', 'SqlConnect', 'source'),
-(43, 'TestSource::tartikel', 'SqlConnect\\Model\\Db\\TableSource', 'sqlconnect/sources/TestSource/show/tartikel', 'SqlConnect', 'source'),
-(44, 'TestTarget::articles', 'SqlConnect\\Model\\Db\\TableTarget', 'sqlconnect/targets/TestTarget/show/articles', 'SqlConnect', 'target');
+(2, 'Digits', 'Ginger\\Model\\Feature\\ValidatorFeature', '#', 'Ginger.Application', 'feature'),
+(3, 'StringTrim', 'Ginger\\Model\\Feature\\FilterFeature', '#', 'Ginger.Application', 'feature'),
+(4, 'AttributeMap', 'Ginger\\Model\\Feature\\AttributeMapFeature', '#', 'Ginger.Application', 'feature'),
+(5, 'DocumentStructureMapper', 'Ginger\\Model\\Mapper\\DocumentStructureMapper', '#', 'Ginger.Application', 'mapper'),
+(6, 'File', 'Ginger\\Model\\File\\SourceFile', '#', 'Ginger.Application', 'source'),
+(7, 'Null', 'Ginger\\Model\\Feature\\FilterFeature', '#', 'Ginger.Application', 'feature'),
+(8, 'Directory', 'Ginger\\Model\\Directory\\SourceDirectory', '#', 'Ginger.Application', 'source'),
+(9, 'Manipulator::StaticValue', 'Ginger\\Model\\Feature\\StaticValueFeature', '#', 'Ginger.Application', 'feature'),
+(10, 'Url', 'Ginger\\Model\\Feature\\FilterFeature', '#', 'Ginger.Application', 'feature'),
+(11, 'PHP Script', 'Ginger\\Model\\Script\\SourceScript', '#', 'Ginger.Application', 'source'),
+(12, 'Dev/Null', 'Ginger\\Model\\Script\\DevNullTarget', '#', 'Ginger.Application', 'target'),
+(13, 'Directory', 'Ginger\\Model\\Directory\\TargetDirectory', '#', 'Ginger.Application', 'target'),
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
